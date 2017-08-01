fc1cc6efe9ec61ef87a4913ca2fc99c8c4865573
Adwait <avabhyankar22@gmail.com>
2017-08-01 09:14:35 +0530

    initial commit with animation

---
+++ Library/src/main/java/adwait/widget/dragcartlib/CirculerRevealView.java
@@ -0,0 +1,112 @@
+package adwait.widget.dragcartlib;
+
+
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewPropertyAnimator;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
+
+/**
+ * Created by adwait on 27/07/17.
+ */
+
+public class CirculerRevealView extends View {
+
+    protected static final int ANIMATION_DURATION = 300;
+
+    protected final Interpolator INTERPOLATOR = new AccelerateDecelerateInterpolator();
+    protected Paint paint = null;
+    protected float expandFraction = 0;
+
+    public CirculerRevealView(Context context) {
+        super(context);
+        init(context, null, -1);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+        paint = new Paint();
+        paint.setColor(context.getResources().getColor(R.color.black));
+    }
+
+    public CirculerRevealView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context,attrs,-1);
+    }
+
+    public CirculerRevealView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context,attrs,defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public CirculerRevealView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context,attrs,defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int cx = canvas.getWidth()/2;
+        int cy = canvas.getHeight()/2;
+        float radius = (float) Math.sqrt(cx*cx + cy*cy) * expandFraction;
+        canvas.drawCircle(cx, cy, radius, paint);
+    }
+
+    public void setColor(int color){
+        paint.setColor(color);
+    }
+
+    public Animator expand(){
+        return animateExpandFraction(0.1f, 1);
+    }
+
+    public Animator expand(float from, float to){
+        if (from<to) {
+            return animateExpandFraction(from, to);
+        }
+        return expand();
+    }
+
+    public Animator contract(){
+        return animateExpandFraction(1, 0.1f);
+    }
+
+    public Animator contract(float from, float to){
+        if (from>to) {
+            return animateExpandFraction(from, to);
+        }
+        return contract();
+    }
+
+    private Animator animateExpandFraction(float from, float to) {
+        ValueAnimator animator = ValueAnimator.ofFloat(from, to);
+        animator.setDuration(ANIMATION_DURATION);
+        animator.setInterpolator(INTERPOLATOR);
+
+        animator.addUpdateListener(updateListener);
+        return animator;
+    }
+
+    protected ValueAnimator.AnimatorUpdateListener updateListener = new ValueAnimator.AnimatorUpdateListener() {
+        @Override
+        public void onAnimationUpdate(ValueAnimator animation) {
+            setExpandFraction((float) animation.getAnimatedValue());
+        }
+    };
+
+    public void setExpandFraction(float expandFraction) {
+        this.expandFraction = expandFraction;
+        invalidate();
+    }
+
+}
